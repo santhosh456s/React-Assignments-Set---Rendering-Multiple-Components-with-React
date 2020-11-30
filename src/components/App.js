b9c92be8cf94510f0cc641efdf25b1a7dc269f35
santhosh <68135777+santhosh456s@users.noreply.github.com>
2020-11-30 15:14:44 +0530

    default

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">blog</h1>
+      <p dara-ns-test="project-description">
+        every thik you know auout technology
+      </p>
+    </div>
+  );
+};
 
 export default App;
