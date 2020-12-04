2e0f3efa33a5356c7c6b8d8857fb3f9a8c970981
santhosh <68135777+santhosh456s@users.noreply.github.com>
2020-12-04 21:31:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main">
-      <h1 data-ns-test="project-name">blog</h1>
-
-      <p dara-ns-test="project-description">
-        every thik you know auout technology
+   
+     <div id="main">
+      <h1 data-ns-test="project-name"> Tic Tac Toe </h1>
+      <p data-ns-test="project-description">
+        this is a two player game in which players have moves one by one and
+        they will be having either X or O . when three consicutive boxes will
+        match horizontally or vertically or diagonally , that player wins first{" "}
       </p>
     </div>
+   
   );
 };
 export default App;
